25435ebbf7de954e49ba814aa651085dc4f7ffc8
kandd871 <114494325+kandd871@users.noreply.github.com>
2022-11-08 00:40:49 -0500

    entry8

---
+++ entries/entry8/main.js
@@ -0,0 +1,55 @@
+function error() {
+  alert("ERROR. TRY AGAIN.");
+}
+
+
+document.querySelector('.btn1').addEventListener('click', showBtn1); 
+document.querySelector('.btn2').addEventListener('click', showBtn2); 
+document.querySelector('.btn3').addEventListener('click', showBtn3); 
+document.querySelector('.btn4').addEventListener('click', showBtn4); 
+document.querySelector('.btn5').addEventListener('click', showBtn5); 
+document.querySelector('.btn6').addEventListener('click', showBtn6);
+document.querySelector('.btn7').addEventListener('click', showBtn7); 
+document.querySelector('.btn8').addEventListener('click', showBtn8); 
+
+
+document.querySelector('.btn2').style.display = 'none';
+document.querySelector('.btn3').style.display = 'none'; 
+document.querySelector('.btn4').style.display = 'none'; 
+document.querySelector('.btn5').style.display = 'none'; 
+document.querySelector('.btn6').style.display = 'none'; 
+document.querySelector('.btn7').style.display = 'none'; 
+document.querySelector('.btn8').style.display = 'none'; 
+ 
+function showBtn1(e) { 
+ document.querySelector('.btn2').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn2(e) { 
+ document.querySelector('.btn3').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn3(e) { 
+ document.querySelector('.btn4').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn4(e) { 
+ document.querySelector('.btn5').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn5(e) { 
+ document.querySelector('.btn6').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn6(e) { 
+ document.querySelector('.btn7').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn7(e) { 
+ document.querySelector('.btn8').style.display = 'block'; 
+ e.preventDefault(); 
+} 
+function showBtn8(e) { 
+ document.querySelector('.buttons').style.display = 'none'; 
+ e.preventDefault(); 
+} 
