b10aed487e48635d0f35fb613fa87d116b958d1a
kandd871 <114494325+kandd871@users.noreply.github.com>
2022-11-15 17:47:59 -0500

    entry9

---
+++ entries/entry 9/main.js
@@ -0,0 +1,49 @@
+const audio = new Audio("audio.mp3");
+let color = ["#a038f0"]
+var video = document.querySelector(".video")
+var eye = document.querySelector("#eye");
+var nose = document.querySelector("#nose")
+var ear = document.querySelector("#ear")
+var hand = document.querySelector ("#hand")
+var lips = document.querySelector ("#lips")
+
+function music() {
+  audio.play();
+  document.body.style.backgroundColor = "black";
+  document.body.style.backgroundImage = "none";
+  video.style.opacity = "0";
+
+}
+
+function feel(){
+  audio.pause();
+  document.body.style.backgroundImage = "url(feel.jpeg)";
+  video.style.opacity = "0";
+}
+
+function taste(){
+  audio.pause();
+  document.body.style.backgroundImage = "url(food.png)";
+  document.body.style.backgroundSize = "contain";
+  video.style.opacity = "0";
+}
+
+function sight(){
+  audio.pause();
+  document.body.style.backgroundImage = "url(eye.gif)";
+  document.body.style.backgroundSize = "contain";
+  document.body.style.backgroundRepeat = "no-repeat";
+  video.style.opacity = "0";
+}
+
+function smell(){
+  audio.pause();
+  video.style.opacity = "1";
+  document.body.style.backgroundImage = "none";
+}
+
+eye.addEventListener("click", music);
+nose.addEventListener("click", feel);
+ear.addEventListener("click", taste);
+hand.addEventListener("click", sight)
+lips.addEventListener("click", smell);
